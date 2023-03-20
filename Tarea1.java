595a446d746b37c7ce4bfb049186744bc89b4eff
Angelcha18 <128178302+Angelcha18@users.noreply.github.com>
2023-03-20 16:04:14 -0500

    Add files via upload

---
+++ Tarea1.java
@@ -0,0 +1,15 @@
+public class operadores {
+    public static void main(String[] args) {
+            int a = 15;
+            int b = 260;
+            int c = 45;
+            
+            suma (a, b, c);
+        }
+    
+        private static void suma(int a, int b, int c){
+             int sumar;
+             sumar = a + b + c;
+             System.out.println(sumar);
+        }
+    }  
